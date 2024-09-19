a712cfd2f079417a93f3f05346836c2a6f5935cc
unknown <Triet Khuong>
2024-09-20 01:15:05 +0700

    Second commit

---
+++ src/entities/Player.java
@@ -0,0 +1,216 @@
+package entities;
+
+import static utilz.Constants.PlayerConstants.*;
+import static utilz.HelpMethods.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import main.Game;
+import utilz.LoadSave;
+
+public class Player extends Entity {
+    private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpeed = 10;
+    private int playerAction = IDLE;
+    private boolean moving = false, attacking = false;
+    private boolean left, up, right, down, jump;
+    private float playerSpeed = 1 * Game.SCALE;
+    private int[][] lvlData;
+    private float xDrawOffset = 11 * Game.SCALE;
+    private float yDrawOffset = 11 * Game.SCALE;
+
+//    Jumping / Gravity
+    private float airSpeed = 0f;
+    private float gravity = 0.04f * Game.SCALE;
+    private float jumpSpeed = -2.25f * Game.SCALE;
+    private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
+    private boolean inAir = false;
+
+    public Player(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        loadAnimations();
+        initHitbox(x * Game.SCALE, y * Game.SCALE, 26 * Game.SCALE, 30 * Game.SCALE);
+    }
+
+    public void update() {
+        updatePos();
+        updateAnimationTick();
+        setAnimation();
+    }
+
+    public void render(Graphics g) {
+        g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset), width, height, null);
+        //Show hit box
+//        drawHitbox(g);
+
+    }
+
+    private void updateAnimationTick() {
+
+        aniTick++;
+        if (aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= GetSpriteAmount(playerAction)) {
+                aniIndex = 0;
+                attacking = false;
+            }
+        }
+    }
+
+    private void setAnimation() {
+
+        int startAni = playerAction;
+
+        if (moving)
+            playerAction = RUNNING;
+        else
+            playerAction = IDLE;
+
+        if(inAir) {
+//            if(airSpeed < 0)
+                playerAction = JUMPING;
+//            else playerAction = FALLING;
+
+        }
+
+        if (attacking)
+            playerAction = RUNNING;
+
+        if (startAni != playerAction)
+            resetAniTick();
+    }
+
+    private void resetAniTick() {
+        aniTick = 0;
+        aniIndex = 0;
+    }
+
+    private void updatePos() {
+        moving = false;
+
+        if(jump)
+            jump();
+        if(!left && !right && !inAir)
+            return;
+
+        float xSpeed = 0;
+
+        if (left && !right)
+            xSpeed -= playerSpeed;
+
+        if (right && !left)
+            xSpeed += playerSpeed;
+
+        if(!inAir)
+            if(!IsEntityOnFloor(hitbox, lvlData))
+                inAir = true;
+
+        if(inAir) {
+
+            if(CanMoveHere(hitbox.x, hitbox.y + airSpeed, hitbox.width, hitbox.height, lvlData)) {
+                hitbox.y += airSpeed;
+                airSpeed += gravity;
+                updateXPos(xSpeed);
+
+            } else {
+                hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, airSpeed);
+                if(airSpeed > 0)
+                    resetInAir();
+                else
+                    airSpeed = fallSpeedAfterCollision;
+                updateXPos((xSpeed));
+
+            }
+
+        } else
+            updateXPos(xSpeed);
+        moving = true;
+    }
+
+    private void jump() {
+        if(inAir)
+            return;
+        inAir = true;
+        airSpeed = jumpSpeed;
+
+    }
+
+    private void resetInAir() {
+        inAir = false;
+        airSpeed = 0;
+
+    }
+
+    private void updateXPos(float xSpeed) {
+        if(CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.x += xSpeed;
+        } else {
+            hitbox.x = GetEntityXPosNextToWall(hitbox, xSpeed);
+        }
+    }
+
+
+    private void loadAnimations() {
+            BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+
+            animations = new BufferedImage[6][10];
+            for (int j = 0; j < animations.length; j++)
+                for (int i = 0; i < animations[j].length; i++)
+                    animations[j][i] = img.getSubimage((i+1)*135, (j+1)*135, 135, 135);
+
+    }
+
+    public void loadLvlData(int[][] lvlData) {
+        this.lvlData = lvlData;
+        if(!IsEntityOnFloor(hitbox, lvlData))
+            inAir = true;
+    }
+
+    public void resetDirBooleans() {
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+    }
+
+    public void setAttacking (boolean attacking) {
+        this.attacking = attacking;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setJump(boolean jump) {
+        this.jump = jump;
+    }
+}
---
+++ src/utilz/LoadSave.java
@@ -0,0 +1,52 @@
+package utilz;
+
+import main.Game;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LoadSave {
+    public static final String PLAYER_ATLAS = "cubes-01.png";
+    public static final String LEVEL_ATLAS = "platforms-01.png";
+    public static final String LEVEL_ONE_DATA = "level_one_data.png";
+
+    public static BufferedImage GetSpriteAtlas(String fileName) {
+        BufferedImage img = null;
+        InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+        try {
+            img = ImageIO.read(is);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return img;
+    }
+
+    public static int[][] GetLevelData() {
+        int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+        BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+
+        System.out.println("Map size: " + img.getHeight() + " : " + img.getWidth());
+
+
+        for (int j = 0; j < img.getHeight(); j++)
+            for (int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getRed();
+                if (value >= 70)
+                    value = 69;
+                lvlData[j][i] = value;
+            }
+        return lvlData;
+
+    }
+}
